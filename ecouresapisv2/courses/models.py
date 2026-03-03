e81e97d869c35f25cd973d4c1b2f6f1e485fe134
MinhTrong13-ux <danh51340@gmail.com>
2026-03-03 16:36:02 +0700

    THB1

---
+++ ecouresapisv2/courses/models.py
@@ -0,0 +1,40 @@
+from  django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+class User(AbstractUser):
+    pass
+
+class BaseModel(models.Model):
+    active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+class Course(BaseModel):
+    subject = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    image = CloudinaryField(null=True, blank=True) #models.ImageField(upload_to='courses/%Y/%m')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 related_name='courses')
+
+    def __str__(self):
+        return self.subject
+class Lesson(BaseModel):
+    subject = models.CharField(max_length=255)
+    content = RichTextField()
+    image = CloudinaryField(null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = ('subject', 'course')
